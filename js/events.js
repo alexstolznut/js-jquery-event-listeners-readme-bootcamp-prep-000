d91333910731968a475feffd2899d2431da9cfa6
Alex Stolzoff <alexstolznut@hotmail.com>
2019-08-26 01:28:29 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -4,7 +4,7 @@ function getIt() {
 }
 
 function frameIt() {
-  $('img').on('load',function(){$('img').attr('class', 'tasty')});
+  $('img').on('load',function(){$('img').addClass(tasty')});
 }
 
 function pressIt(){
