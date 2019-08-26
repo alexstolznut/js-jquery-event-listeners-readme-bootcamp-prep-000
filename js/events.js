29cde7f93a384bd17560518e958a17e75ab3498d
Alex Stolzoff <alexstolznut@hotmail.com>
2019-08-26 01:20:02 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,26 +1,23 @@
 //define functions here
-const getIt = function() {
-  $('p').on('click', function() {
-    return alert('Hey!')
-  }
-);
+function getIt() {
+  $('p').on('click', function(){ return alert('Hey!')});
 }
 
-const frameIt = function() {
+function frameIt() {
   $('img').on('load', this.addClass('tasty'));
 }
 
-const pressIt = function() {
+function pressIt(){
   $('form > input#typing').on('keydown',(e)=> (e.which === 'G'){alert('You pressed G!')});
 }
 
-const submitIt = function(){
+function submitIt(){
   $('form').on('submit', ()=>alert("Your form is going to be submitted now."));
 }
 $(document).ready(function(){
 // call functions here
-window.getIt();
-window.frameIt();
-window.pressIt();
-window.submitIt();
+getIt();
+frameIt();
+pressIt();
+submitIt();
 });
