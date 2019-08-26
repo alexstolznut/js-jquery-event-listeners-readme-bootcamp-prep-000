cb505874faac53a8e3e3682ecf11689866863284
Alex Stolzoff <alexstolznut@hotmail.com>
2019-08-26 01:15:52 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -15,10 +15,9 @@ function submitIt(){
   $('form').on('submit', ()=>alert("Your form is going to be submitted now."));
 }
 $(document).ready(function(){
+// call functions here
 getIt();
 frameIt();
 pressIt();
 submitIt();
-// call functions here
-
 });
