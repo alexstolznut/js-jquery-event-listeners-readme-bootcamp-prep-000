bd947eba8e4b316691f8daece66ed460348baf58
Alex Stolzoff <alexstolznut@hotmail.com>
2019-08-26 01:14:17 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,5 +1,19 @@
 //define functions here
+function getIt() {
+  $('p').on('click', ()=>alert('Hey!'));
+}
 
+function frameIt() {
+  $('img').on('load', this.addClass('tasty'));
+}
+
+function pressIt(){
+  $('form > input#typing').on('keydown',(e)=> (e.which === 'G'){alert('You pressed G!')});
+}
+
+function submitIt(){
+  $('form').on('submit', ()=>alert("Your form is going to be submitted now."));
+}
 $(document).ready(function(){
 
 // call functions here
